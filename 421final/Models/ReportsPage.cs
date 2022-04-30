4d0dfabb5bf64d57bdf48dd56ec4d3a939894c99
Ethan Basenberg <embasenberg@crimson.ua.edu>
2022-04-30 12:38:46 -0500

    added the reports model, view, and controller, styled the comparision page, created full css files for report.css and site.css, added javascript functionality to reports page through site.js, added reports tab to all layouts

---
+++ 421final/Models/ReportsPage.cs
@@ -0,0 +1,19 @@
+﻿namespace _421final.Models
+{
+    public class ReportsPage
+    {
+
+        public List<PlayerRoot> playerList = new List<PlayerRoot>();
+
+        public List<TeamRoot> teamList = new List<TeamRoot>();
+
+
+        public List<PlayerRoot> HeightList = new List<PlayerRoot>();
+
+        public List<PlayerRoot> WeightList = new List<PlayerRoot>();
+
+        public List<TeamRoot> ChampionList = new List<TeamRoot>();
+        public List<TeamRoot> NoChampionList = new List<TeamRoot>();
+
+    }
+}
--- 421final/wwwroot/js/site.js
+++ 421final/wwwroot/js/site.js
@@ -2,3 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+
+
+
+function getChampionships() {
+        document.getElementById("one").removeAttribute("hidden");
+        document.getElementById("two").removeAttribute("hidden");
+        document.getElementById("three").removeAttribute("hidden");
+}
+
+
+function getNoChampionships() {
+    document.getElementById("four").removeAttribute("hidden");
+    document.getElementById("five").removeAttribute("hidden");
+    document.getElementById("six").removeAttribute("hidden");
+}
+
+
+
+function getTallGuys() {
+    document.getElementById("seven").removeAttribute("hidden");
+    document.getElementById("eight").removeAttribute("hidden");
+    document.getElementById("nine").removeAttribute("hidden");
+}
+
+
+
+function getLightGuys() {
+    document.getElementById("ten").removeAttribute("hidden");
+    document.getElementById("eleven").removeAttribute("hidden");
+    document.getElementById("twelve").removeAttribute("hidden");
+}
